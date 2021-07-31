61a16fc2f362ffccffde0c9fc2d69bad0b2351d5
mahakamal1 <mahakamal150@gmail.com>
2021-07-31 11:52:49 +0200

    adding home component

---
+++ src/app/components/Home/fourthsection/fourthsection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-fourthsection',
+  templateUrl: './fourthsection.component.html',
+  styleUrls: ['./fourthsection.component.css']
+})
+export class FourthsectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
