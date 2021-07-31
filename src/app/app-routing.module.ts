895e207ce316068b368e1c0206b653d24885fd32
hsn656 <hassan.mhmd656@gmail.com>
2021-07-31 07:49:59 -1000

    Lesson Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AnswersComponent } from './components/answers/answers.component';
 import { ErrorComponent } from './components/error/error.component';
 import { ExamComponent } from './components/exam/exam.component';
 import { MasterpageComponent } from './components/Home/masterpage/masterpage.component';
+import { LessonComponent } from './components/lesson/lesson.component';
 import { QuestionsComponent } from './components/questions/questions.component';
 
 const routes: Routes = [
@@ -12,12 +13,13 @@ const routes: Routes = [
   {path:"answers/:id", component:AnswersComponent},
   {path:"Home", component:MasterpageComponent},
   {path:"exam/:id", component:ExamComponent},
+  {path:"lesson/:id", component:LessonComponent},
   {path:"**", component:ErrorComponent}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes,{
-  scrollPositionRestoration: 'enabled'
+scrollPositionRestoration: 'enabled'
   })],
   exports: [RouterModule]
 })
--- src/app/components/lesson/lesson.component.ts
+++ src/app/components/lesson/lesson.component.ts
@@ -1,4 +1,7 @@
 import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { LessonService } from './../../../services/lesson.service';
 
 @Component({
   selector: 'app-lesson',
@@ -8,22 +11,34 @@ import { AfterViewInit, Component, OnInit, ViewChild } from '@angular/core';
 export class LessonComponent implements OnInit,AfterViewInit {
 
   @ViewChild("video") videoUrl:any;
+  id:number;
 
-  lesson:any={
-    id:1,
-    name:"ازاي تتعامل مع القطعة صح في عشر دقايق",
-    url:"k6o8H4YMwGE"
-  }
 
-  constructor() { }
+  lesson:any;
+  private subscriptions: Subscription[] = [];
+
+
+  constructor(private lsnService:LessonService,private route:ActivatedRoute) {
+    this.id = route.snapshot.params["id"];
+   }
 
   ngOnInit(): void {
+    var subscription =  this.lsnService.getByID(this.id).subscribe(
+      (res)=>{this.lesson=res;
+        let link = "https://www.youtube.com/embed/"+"k6o8H4YMwGE"+"?controls=1&fs=1&rel=0"
+        this.videoUrl.nativeElement.innerHTML =`<iframe height="100%" width="100%" src=${link}>
+        </iframe>`
+      },
+      (err)=>console.log(err)
+    )
+    console.log("done");
+
+    this.subscriptions.push(subscription)
   }
 
   ngAfterViewInit(): void {
-    let link = "https://www.youtube.com/embed/"+this.lesson.url+"?controls=1&fs=1&rel=0"
-    this.videoUrl.nativeElement.innerHTML =`<iframe height="100%" width="100%" src=${link}>
-    </iframe>`
+
     }
+
 
 }
