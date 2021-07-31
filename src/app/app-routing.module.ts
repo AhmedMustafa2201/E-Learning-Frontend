c4f3fd78119bcb6fc2d0992defeb88df16ae2fde
mahakamal1 <78565941+mahakamal1@users.noreply.github.com>
2021-07-31 11:36:28 +0200

    Add files via upload
    adding index to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AnswersComponent } from './components/answers/answers.component';
 import { ErrorComponent } from './components/error/error.component';
+import { MasterpageComponent } from './components/Home/masterpage/masterpage.component';
 import { QuestionsComponent } from './components/questions/questions.component';
 
 const routes: Routes = [
-  {path:"", redirectTo:"questions", pathMatch:"full"},
+  {path:"", redirectTo:"Home", pathMatch:"full"},
   {path:"questions", component:QuestionsComponent},
   {path:"answers/:id", component:AnswersComponent},
+  {path:"Home", component:MasterpageComponent},
   {path:"**", component:ErrorComponent}
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,13 +8,33 @@ import { ErrorComponent } from './components/error/error.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 import { AnswersComponent } from './components/answers/answers.component';
+import { HeaderComponent } from './components/Shared/header/header.component';
+import { FooterComponent } from './components/Shared/footer/footer.component';
+import { FirstsectionComponent } from './components/Home/firstsection/firstsection.component';
+import { MasterpageComponent } from './components/Home/masterpage/masterpage.component';
+import { SecondsectionComponent } from './components/Home/secondsection/secondsection.component';
+import { ThiredsectionComponent } from './components/Home/thiredsection/thiredsection.component';
+import { FourthsectionComponent } from './components/Home/fourthsection/fourthsection.component';
+import { SectionfiveComponent } from './components/Home/sectionfive/sectionfive.component';
+import { SectionsixComponent } from './components/Home/sectionsix/sectionsix.component';
+import { SectionsevenComponent } from './components/Home/sectionseven/sectionseven.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     QuestionsComponent,
     ErrorComponent,
-    AnswersComponent
+    AnswersComponent,
+    HeaderComponent,
+    FooterComponent,
+    FirstsectionComponent,
+    MasterpageComponent,
+    SecondsectionComponent,
+    ThiredsectionComponent,
+    FourthsectionComponent,
+    SectionfiveComponent,
+    SectionsixComponent,
+    SectionsevenComponent
   ],
   imports: [
     BrowserModule,
