eaad4bb57054c3a736f090a376c5e468b4c120ac
ahmed_mustafa <program22b@gmail.com>
2021-08-02 15:05:00 +0200

    Posts and comments fronts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,7 +7,16 @@ import * as AOS from 'aos';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit  {
+  obj:string[]=["Admin", "Guest"];
   ngOnInit(): void {
     AOS.init();
+
+    // fake logged in localstorage
+    localStorage.setItem("rnid", "99fb5a77-55d4-453f-8434-a432b0a8895c")
+    localStorage.setItem("rntoken", "a token goes here")
+    localStorage.setItem("rnemail", "AhmedMustafa@gmail.com")
+    localStorage.setItem("rnroles", JSON.stringify(this.obj))
+    localStorage.setItem("rnexpireson", "Sun, 01 Aug 2021 10:29:16 GMT")
+    localStorage.setItem("rnisauthinticated", "true")
   }
 }
--- src/app/components/answers/answers.component.ts
+++ src/app/components/answers/answers.component.ts
@@ -1,171 +1,235 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
-import { AnswerService } from 'src/services/answer.service';
 import { QuestionService } from 'src/services/question.service';
 import * as moment from 'moment';
 import { Subscription } from 'rxjs';
+import { Post } from 'src/app/Models/post';
+import { Comment } from 'src/app/Models/comment';
+import { AnswerService } from 'src/services/answer.service';
 
 @Component({
   selector: 'app-answers',
   templateUrl: './answers.component.html',
-  styleUrls: ['./answers.component.css']
+  styleUrls: ['./answers.component.css'],
 })
-export class AnswersComponent implements OnInit, OnDestroy {
-  id:number=0
+export class AnswersComponent implements AfterViewInit, OnDestroy, OnInit {
+  id: number = 0;
   private subscriptions: Subscription[] = [];
 
-  allAnswers:any=[]
-  questions:any=[]
-  otherAnswers:any=[]
+  // allAnswers: any = [];
+  questions: Post;
+  otherAnswers: Comment[];
 
-  bestAnswer:any
-  questionDetails:any
-  answer:string=""
+  bestAnswer: Comment;
+  answer: string = '';
 
-  constructor(private myService: AnswerService, private qService: QuestionService,
-    activated:ActivatedRoute, private router: Router) {
-    this.id = activated.snapshot.params["id"];
-    this.router.routeReuseStrategy.shouldReuseRoute = () => false;
+  asideQuestions:Post[]
+
+  constructor(
+    private qService: QuestionService,
+    private myService: AnswerService,
+    private activated: ActivatedRoute,
+    private router: Router
+  ) {
+    this.activated.paramMap.subscribe(params => {
+      this.id = parseInt(params.get('id'));
+      this.ngOnInit();
+    });
+    // this.id = activated.snapshot.params['id'];
+
+
+  }
+  ngOnInit(): void {
+    this.getquestionWithAnswers();
+    this.getquestionsWithLimited()
+  }
+
+  ngAfterViewInit(): void {
+    this.setViewCount(this.id);
   }
   ngOnDestroy(): void {
-    for(let sub of this.subscriptions){
-      sub.unsubscribe()
+    for (let sub of this.subscriptions) {
+      sub.unsubscribe();
     }
   }
-
-  ngOnInit(): void {
-    this.getQuestionDetail(this.id)
-    this.getAllData(this.id)
-    this.getquestions()
+  checkToken(){
+    return localStorage.getItem("rntoken")
+  }
+  getquestionWithAnswers() {
+    this.subscriptions.push(
+      this.qService.getByID(this.id).subscribe(
+        (res) => {
+          this.questions = res as Post
+          this.bestAnswer = this.questions.comments.sort(this.sortArray)[0];
+          this.otherAnswers = this.questions.comments
+          this.otherAnswers.shift()
+        },
+        (err) => console.error(err)
+      )
+    );
   }
 
-  getquestions(){
+
+  getquestionsWithLimited() {
     this.subscriptions.push(
-    this.qService.getAllWithLimited().subscribe(
-      (res)=>this.questions=res,
-      (err)=>console.error(err)
-    ))
+      this.qService.getAllWithLimited().subscribe(
+        (res) => {
+          this.asideQuestions = res as Post[]
+        },
+        (err) => console.error(err)
+      )
+    );
+  }
+
+  sortArray(a:Comment, b:Comment):number{
+      // return a.commentLikes - b.commentLikes
+      if (a.commentLikes > b.commentLikes) {
+        return -1;
+      }
+      if (a.commentLikes < b.commentLikes) {
+        return 1;
+      }
+      return 0;
   }
 
   getEstimatedDate(date: string): string {
     return moment(date).locale('ar').fromNow();
   }
 
-  getAllData(id:number){
+  setViewCount(id: number) {
+    let question = [
+      {
+        value: this.questions.views + 1,
+        path: "/views",
+        op: "replace",
+      }
+    ];
     this.subscriptions.push(
-    this.myService.getAllByQuestion(id).subscribe(
-      (res)=>{
-        this.allAnswers=res
-        this.bestAnswer=this.allAnswers.sort(function (a: any, b: any) {
-          return a.evaluation<b.evaluation
-        })[0]
-
-        this.otherAnswers = this.allAnswers.filter( (p: any) => {
-          return p.id != this.bestAnswer.id;
-        });
-        // this.otherAnswers.pop()
-      },
-      (err)=>console.error(err)
-    ))
+      this.qService.updateViewCount(id, question).subscribe(
+        (res) => console.log("done!!!"),
+        (err) => console.error(err)
+      )
+    );
   }
-
-  getQuestionDetail(id:number){
-    this.subscriptions.push(
-    this.qService.getByID(id).subscribe(
-      (res)=>{
-        this.questionDetails=res
-      },
-      (err)=>console.error(err)
-    ))
-  }
-
 
   increaseEval(up: HTMLElement, down: HTMLElement) {
     if (!up.classList.contains('text-muted')) {
-      this.questionDetails.evaluation++
-      this.qService.update(this.id, this.questionDetails).subscribe(
+      let like={
+        postId: this.id,
+        userId: localStorage.getItem("rnid")
+      }
+      this.subscriptions.push(
+      this.qService.updateLike(this.id, like).subscribe(
         (res) => {
-          up.classList.add('text-muted');
-          down.classList.remove('text-muted');
+          this.getquestionWithAnswers()
+          // up.classList.add('text-muted');
+          // down.classList.remove('text-muted');
         },
         (err) => console.error(err)
-      );
+      ))
     }
-    console.log(this.questionDetails)
   }
 
   decreaseEval(up: HTMLElement, down: HTMLElement) {
     if (!down.classList.contains('text-muted')) {
-
-      if (this.questionDetails.evaluation != 0) {
-        this.questionDetails.evaluation--;
-        this.qService.update(this.id, this.questionDetails).subscribe(
-          (res) => {
-            down.classList.add('text-muted');
-            up.classList.remove('text-muted');
-          },
-          (err) => console.error(err)
-        );
+      let like={
+        postId: this.id,
+        userId: localStorage.getItem("rnid")
       }
-    }
-  }
-  ////////////////////////////////////////////////////
-  increaseAnsEval(id:number, up: HTMLElement, down: HTMLElement){
-    debugger
-    let ss = this.allAnswers.filter(function (p: any) {
-      return p.id == id;
-    });
-    console.log(ss)
-    if (!up.classList.contains('text-muted')) {
-      ss[0].evaluation++
-      this.myService.update(id, ss[0]).subscribe(
+      this.subscriptions.push(
+      this.qService.updateDisLike(this.id, like).subscribe(
         (res) => {
-          up.classList.add('text-muted');
-          down.classList.remove('text-muted');
+          this.getquestionWithAnswers()
+
+          // down.classList.add('text-muted');
+          // up.classList.remove('text-muted');
         },
         (err) => console.error(err)
-      );
+      ))
+    }
+
+  }
+  ////////////////////////////////////////////////////
+  increaseAnsEval(id: number, up: HTMLElement, down: HTMLElement) {
+    if (!up.classList.contains('text-muted')) {
+      let like={
+        commentId: id,
+        userId: localStorage.getItem("rnid")
+      }
+      this.subscriptions.push(
+      this.myService.updateLike(id, like).subscribe(
+        (res) => {
+          this.getquestionWithAnswers()
+
+          // down.classList.add('text-muted');
+          // up.classList.remove('text-muted');
+        },
+        (err) => console.error(err)
+      ))
     }
   }
 
-  decreaseAnsEval(id:number, up: HTMLElement, down: HTMLElement){
+  decreaseAnsEval(id: number, up: HTMLElement, down: HTMLElement) {
     if (!down.classList.contains('text-muted')) {
-      let ss = this.allAnswers.filter(function (p: any) {
-        return p.id == id;
-      });
-
-      if (ss[0].evaluation != 0) {
-        ss[0].evaluation--;
-        this.myService.update(id, ss[0]).subscribe(
-          (res) => {
-            down.classList.add('text-muted');
-            up.classList.remove('text-muted');
-          },
-          (err) => console.error(err)
-        );
+      let like={
+        commentId: id,
+        userId: localStorage.getItem("rnid")
       }
+      this.subscriptions.push(
+      this.myService.updateDisLike(id, like).subscribe(
+        (res) => {
+          this.getquestionWithAnswers()
+
+          // down.classList.add('text-muted');
+          // up.classList.remove('text-muted');
+        },
+        (err) => console.error(err)
+      ))
     }
   }
   ////////////////////////////////////////////////////
-  AddData(){
-    let date=new Date()
-    let answerObj:any={
-      content:this.answer,
-      createdAt: date.toUTCString(),
-      evaluation:0,
-      questionID:this.id,
-      user: {
-        id: 1,
-        name: 'hsn mohammed',
-        photo:
-          'https://lh3.googleusercontent.com/a-/AOh14GivB7p6yl_U3CC8bDpmPJ-Yixf3nDgYMLaVHgUc=s96-c',
-      }
+  currentUserLike(data:Post){
+    if(data.postLikesArr.length==0||data.postLikesArr==undefined||data.postLikes==0){
+      return false
     }
+    return data.postLikesArr.includes(localStorage.getItem("rnid"))
+  }
+
+  currentUserDisLike(data:Post){
+    if(data.postDisLikesArr.length==0||data.postDisLikesArr==undefined){
+      return false
+    }
+    return data.postDisLikesArr.includes(localStorage.getItem("rnid"))
+  }
+
+  ////////////////////////////////////////////////////
+  currentUserCommentLike(data:Comment){
+    if(data.commentLikesArr.length==0||data.commentLikesArr==undefined||data.commentLikes==0){
+      return false
+    }
+    return data.commentLikesArr.includes(localStorage.getItem("rnid"))
+  }
+
+  currentUserCommentDisLike(data:Comment){
+    if(data.commentDisLikesArr.length==0||data.commentDisLikesArr==undefined||data.commentLikes==0){
+      return false
+    }
+    return data.commentDisLikesArr.includes(localStorage.getItem("rnid"))
+  }
+  ////////////////////////////////////////////////////
+
+
+  AddData() {
+    let answerObj: any = {
+      content: this.answer,
+      postId: this.id,
+      userId: localStorage.getItem("rnid")
+    };
 
     this.myService.add(answerObj).subscribe(
       (res)=>{
-        console.log(res)
-        this.getAllData(this.id)
+        // console.log(res)
+        this.getquestionWithAnswers()
       },
       (err)=>console.error(err)
     )
